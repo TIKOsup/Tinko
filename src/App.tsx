22ff65e52be4b4c33dc2aaf5b3780753f95f03a4
Temirlan <tiko@Temirlans-MacBook-Pro.local>
2026-02-12 21:43:24 +0500

    feat: add tailwind and shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
+import { Button } from "@/components/ui/button"
 
 function App() {
   return (
-    <div className="app">
-      {/* Start building your React app here */}
-    </div>
+    <>
+      <div className="app flex flex-col items-center justify-center gap-4">
+        <h1>TEST</h1>
+        <Button>Btn test</Button>
+      </div>
+    </>
   )
 }
 
