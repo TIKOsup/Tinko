c48b374a555981afebde18ac8f8b8f67a759e4a3
Temirlan <tiko@Temirlans-MacBook-Pro.local>
2026-03-24 12:40:26 +0500

    feat: add new file page design

--- src/components/doc/file/file.tsx
+++ src/components/doc/file/file.tsx
@@ -1,10 +1,4 @@
-import { AppSidebar } from '@/components/sidebar/app-sidebar'
 import { Separator } from '@/components/ui/separator'
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from '@/components/ui/sidebar'
 import Search from '@/components/search/search'
 import { useParams } from 'react-router-dom'
 import data from '@/data/doc.json'
@@ -17,24 +11,21 @@ export default function File() {
   }
 
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <header className='flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12'>
-          <div className='flex w-full items-center gap-2 px-4'>
-            <SidebarTrigger className='-ml-1' />
-            <Separator
-              orientation='vertical'
-              className='mr-2 data-[orientation=vertical]:h-4'
-            />
-            <p>Place for breadcrumb</p>
-            <div className='ml-auto'>
-              <Search />
-            </div>
+    <div className='w-full overflow-x-hidden'>
+      <header className='w-full border-b'>
+        <div className='mx-auto flex h-16 max-w-7xl items-center gap-2 p-4 border-x'>
+          <div className="flex aspect-square size-10 items-center justify-center">
+            <img src="/logo.png" alt="Project Logo" />
           </div>
-        </header>
-        <main className='flex flex-1 flex-col gap-6 p-4'>
-          <div className='px-6 py-4'>
+          <div className='ml-auto'>
+            <Search />
+          </div>
+        </div>
+      </header>
+
+      <div className='mx-auto flex min-h-screen w-full max-w-7xl flex-col border-x'>
+        <main className='flex w-full flex-1 flex-col gap-6 p-4'>
+          <div className=''>
             <div className='flex flex-col gap-3'>
               <h1 className='text-3xl font-semibold tracking-tight text-foreground'>
                 {data.name}
@@ -43,7 +34,7 @@ export default function File() {
           </div>
 
           <article className='flex flex-1 flex-col gap-4'>
-            <div className='px-6 py-4'>
+            <div className=''>
               <h2 className='text-2xl font-semibold mb-4'>
                 Documentation
               </h2>
@@ -70,7 +61,7 @@ export default function File() {
                     {data.variables.map((variable) => (
                       <div
                         key={variable.name}
-                        className='flex flex-col gap-1'
+                        className='flex flex-col gap-1 min-w-0'
                       >
                         <div className='rounded-md border bg-background bg-gray-50'>
                           <div className='border-b px-2 py-1 text-xs text-right'>
@@ -78,7 +69,7 @@ export default function File() {
                               View Source
                             </a>
                           </div>
-                          <pre className='overflow-x-auto px-4 py-3 text-sm leading-6 text-foreground'>
+                          <pre className='max-w-full overflow-x-auto px-4 py-3 text-sm leading-6 text-foreground'>
                             <code>
                               {`DEFINE VARIABLE ${variable.name} AS ${variable.type}`}
                             </code>
@@ -102,14 +93,14 @@ export default function File() {
                     {data.procedures.map((procedure) => (
                       <div
                         key={procedure.name}
-                        className='flex flex-col gap-1'
+                        className='flex flex-col gap-1 min-w-0'
                       >
                         <h3 className='text-base font-semibold text-foreground'>
                           proc <a href="#" className='text-blue-500 hover:underline'>{procedure.name}</a>
                         </h3>
 
                         <div className='rounded-md border bg-background bg-gray-50'>
-                          <pre className='overflow-x-auto px-4 py-3 text-sm leading-6 text-foreground'>
+                          <pre className='max-w-full overflow-x-auto px-4 py-3 text-sm leading-6 text-foreground'>
                             <code>
                               {`PROCEDURE ${procedure.name} (${procedure.parameters
                                 .map(
@@ -137,14 +128,14 @@ export default function File() {
                     {data.functions.map((fn) => (
                       <div
                         key={fn.name}
-                        className='flex flex-col gap-1'
+                        className='flex flex-col gap-1 min-w-0'
                       >
                         <h3 className='text-base font-semibold text-foreground'>
                           func <a href="#" className='text-blue-500 hover:underline'>{fn.name}</a>
                         </h3>
 
                         <div className='rounded-md border bg-background bg-gray-50'>
-                          <pre className='overflow-x-auto px-4 py-3 text-sm leading-6 text-foreground'>
+                          <pre className='max-w-full overflow-x-auto px-4 py-3 text-sm leading-6 text-foreground'>
                             <code>
                               {`FUNCTION ${fn.name} (${fn.parameters
                                 .map(
@@ -166,7 +157,11 @@ export default function File() {
             </div>
           </article>
         </main>
-      </SidebarInset>
-    </SidebarProvider>
+      </div>
+
+      <footer className='w-full border-t'>
+        <div className='mx-auto h-16 max-w-7xl border-x'></div>
+      </footer>
+    </div>
   )
 }
